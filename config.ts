7d7ce9e7b6f50e2e8abae06cfc4b6325c9af9364
kelvinpompey <kelvin.pompey@gmail.com>
2025-06-26 14:13:48 -0400

    Tweaked config

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 import ExpoConstants from "expo-constants";
 
-export const appURL = process.env.EXPO_PUBLIC_APP_URL;
+export const appURL = "https://skill-dial-dev.expo.app";
 export const supabaseURL = ExpoConstants.expoConfig?.extra
   ?.supabaseURL as string;
 export const supabaseAnonKey = ExpoConstants.expoConfig?.extra
